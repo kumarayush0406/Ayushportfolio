6f42114a39150e07c21e1efbb2437d788a00dc83
kumarayush0406 <112921096+kumarayush0406@users.noreply.github.com>
2025-04-03 14:41:45 +0530

    Update NavbarBtn.jsx

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -4,7 +4,7 @@ const NavbarBtn = () => {
   return (
     <button className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-centre gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale-110 transition-all duration-500 hover:shadow-cyanShadow ">
      <a href="https://drive.google.com/file/d/10aJLoStAgGwX_iX9BU_x3wYszfiY3FDe/view?usp=drivesdk">Resume</a>
-      <div className="!flex justify-center items-center sm:hidden md:block">
+      <div className="sm:hidden md:block pt-1">
       <RiArrowRightDownLine /></div>
     </button>
   );
