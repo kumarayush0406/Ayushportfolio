0cfd6d105de9cdd8c7c576eba4e76ef570d939ed
kumarayush0406 <112921096+kumarayush0406@users.noreply.github.com>
2025-04-03 13:54:50 +0530

    Update SkillsText.jsx

--- src/components/skillsSection/SkillsText.jsx
+++ src/components/skillsSection/SkillsText.jsx
@@ -4,7 +4,7 @@ const SkillsText = () => {
     return (
         <div  className="flex flex-col items-center mt-[100px]">
             <h2 className="text-6xl text-cyan mb-10 ">My Skills</h2>
-            <p className="text-lg text-center text-white">I not only work these technlogies but excillent in using them with best practices to deliver high quality result , I have been working with all these skills to build my portfolio projects. </p>
+            <p className="text-lg text-center text-white">I not only work these technlogies but excellent in using them with best practices to deliver high quality result , I have been working with all these skills to build my portfolio projects. </p>
         </div>
     )
 }
